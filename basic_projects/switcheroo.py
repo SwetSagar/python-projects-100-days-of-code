e1c8ff1df35c5215bfc47f93e4c73d49693d2dcd
SwetSagar <87872049+SwetSagar@users.noreply.github.com>
2024-02-24 21:00:08 +0530

    day1

---
+++ basic_projects/switcheroo.py
@@ -0,0 +1,18 @@
+a = input()
+b = input()
+
+'''
+Code below stores the value of a temporarily in a variable c
+'''
+
+c = a 
+a = b
+b = c
+
+'''
+Flow of the program is important here 
+
+'''
+
+print("a! " + a)
+print("b! " + b)
