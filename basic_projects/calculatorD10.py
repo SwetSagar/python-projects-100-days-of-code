00cca4500f15b985586486376f7c276cb6ddc9d6
SwetSagar <87872049+SwetSagar@users.noreply.github.com>
2024-03-06 11:20:42 +0530

    commit

---
+++ basic_projects/calculatorD10.py
@@ -0,0 +1,48 @@
+
+import os
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1,n2):
+  return n1 - n2
+
+def multiply(n1,n2):
+  return n1*n2
+
+def divide(n1,n2):
+  return n1/n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  
+  n1 = float(input("What's the first number?: "))
+  
+  for symbol in operations:
+    print(symbol)
+    
+  user_input  = input("What operation do you want to perform? ")
+  
+  calculator_running = True
+  
+  while calculator_running :
+    n2 = float(input("What's the next number?: "))
+    calculation_function = operations[user_input]
+    answer = calculation_function(n1,n2)
+    print(f"{n1} {user_input} {n2} = {answer}")
+  
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      n1 = answer
+    else:
+      calculator_running = False
+      os.system('cls')
+      calculator()
+
+calculator()
+        
