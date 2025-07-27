4cffcdf8ee2c3bbfd55fde5af775c5faa9668546
Swet Sagar <swet.sagar93@gmail.com>
2025-07-27 21:24:39 +0530

    Moving to main

---
+++ basic-projects/folder-creator.py
@@ -0,0 +1,12 @@
+import os
+
+num_days = int(input("Enter the Folder days that you would want to create : "))
+
+for i in range(1, num_days):
+	folder_name = f"day-{i}"
+
+	try : 
+		os.makedirs(folder_name, exist_ok=True)
+		print(f"Created folder: {folder_name}")
+	except Exception as e : 
+		print(f"Error creating folder {folder_name}: {e}")
